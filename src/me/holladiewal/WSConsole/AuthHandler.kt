e8d8a2c2f19676edf3dd86c022e1ee3516ca47f1
beepbeat <zfiremani885@gmail.com>
2017-10-19 18:57:57 +0200

    Initial commit

---
+++ src/me/holladiewal/WSConsole/AuthHandler.kt
@@ -0,0 +1,13 @@
+package me.holladiewal.WSConsole
+
+/**
+ * Created by beepbeat/holladiewal on 18.10.2017.
+ */
+
+class AuthHandler{
+
+    fun auth(){
+
+    }
+
+}
---
+++ src/me/holladiewal/WSConsole/Main.kt
@@ -0,0 +1,9 @@
+package me.holladiewal.WSConsole
+
+object Main {
+
+    //@JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+}
