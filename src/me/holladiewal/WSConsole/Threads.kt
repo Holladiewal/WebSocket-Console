8d91437ec0b2be95a9265972b0067a242aff4d97
beepbeat <zfiremani885@gmail.com>
2017-10-19 21:22:57 +0200

    Added InputThread, added AuthHandler

---
+++ src/me/holladiewal/WSConsole/Threads.kt
@@ -0,0 +1,22 @@
+package me.holladiewal.WSConsole
+
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.launch
+import java.util.*
+
+
+/**
+ * Created by beepbeat/holladiewal on 19.10.2017.
+ */
+class Threads {
+    lateinit var inputJob: Job
+
+    fun startInput(){
+        val inn = Scanner(System.`in`)
+        inputJob = launch{
+            while(!inn.hasNextLine()){}
+            var msg = inn.nextLine()
+            //send
+        }
+    }
+}
